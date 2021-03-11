ae3c5515d09f0ac6226d5a29664562e46f2e898a
Akogwu Emmanuel <akogwu.emmanuel24@gmail.com>
2021-03-11 04:06:02 +0100

    conditionals

---
+++ script.js
@@ -0,0 +1,20 @@
+//Program to determine who is older between a girl and a boy
+const ageOfBoy = 25;
+const ageOfGirl = 21;
+
+if (ageOfBoy == ageOfGirl) {
+  console.log("the boy and the girl are of the same Age");
+} else if (ageOfBoy > ageOfGirl) {
+  console.log("the boy is older than the girl");
+} else {
+  console.log("the girl os older than the boy");
+}
+
+//program to determine the color of a boars
+const boardColor = "black";
+if (boardColor != "white") {
+  //!== also does the same in this case
+  console.log("the board color is black");
+} else {
+  console.log("the board color is white");
+}
