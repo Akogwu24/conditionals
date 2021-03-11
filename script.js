478bc7a6dd3dd919290b873328bc5dd918c5a89c
Akogwu Emmanuel <akogwu.emmanuel24@gmail.com>
2021-03-11 04:10:18 +0100

    conditionls and some array methods

--- script.js
+++ script.js
@@ -18,3 +18,55 @@ if (boardColor != "white") {
 } else {
   console.log("the board color is white");
 }
+
+//x is a variable that contains the data type of 5
+var x = 5;
+
+//increment
+console.log("for variable  x++ where x=5");
+console.log(x);
+console.log(x++);
+console.log(x++);
+console.log(x++);
+console.log(x);
+
+console.log("");
+console.log("for variable ++y where y=5");
+console.log("");
+//Decrement
+var y = 5;
+console.log(y);
+console.log(++y);
+console.log(++y);
+console.log(++y);
+console.log(y);
+
+console.log(" ");
+console.log("array manipulation begibs here");
+console.log(" ");
+//arrays
+const array = [22, "Banana", "Orange", "Apple", "Mango", 3, "SUA"];
+console.log(array);
+
+//pop to remove the last item in the array
+const arrayPoped = array.pop();
+console.log(arrayPoped);
+
+//push adds item to the end of the array
+array.push("samsung");
+console.log(array);
+
+//reversed array
+const reversedArray = array.reverse();
+console.log(reversedArray);
+
+//conberts to non-arrays
+console.log(array.toString("-"));
+
+//adds to items from the begining
+array.unshift("man o war");
+console.log(array);
+
+//remove item from the beginig
+array.shift();
+console.log(array);
